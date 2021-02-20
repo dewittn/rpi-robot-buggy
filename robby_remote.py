5641437eeb6c5ada4f9649b7ec1022c09606062d
Nelson/Roberto <hello@nelsonroberto.com>
2021-02-20 13:07:37 -0500

    Updated robby_remote.py
    Cleaned up code

--- robby_remote.py
+++ robby_remote.py
@@ -1,21 +1,20 @@
 # Code to run the buggy from a remote Pi using a home-brew game controller.
 # https://projects.raspberrypi.org/en/projects/remote-control-buggy/4
-from gpiozero import Robot, Button, LED
+from gpiozero import Robot, Button, LED, Device
 from gpiozero.pins.pigpio import PiGPIOFactory
 from signal import pause
 import time
 
 # There is a bug in GPIOZero 1.5.1 that prevents pin_factory from working with Robot and Motor
-# This code does not work!
+# For example, this code does not work!
 # factory = PiGPIOFactory(host='192.168.1.17')
 # robot = Robot(left=(4, 14), right=(17, 18), pin_factory=factory)  # remote pins
 
 # To address this bug we set the global pin factory to the remote host
 # and connect to the breadboard controller through a localhost pin_factory.
-import gpiozero
-gpiozero.Device.pin_factory = PiGPIOFactory(host='192.168.7.96')
-
+Device.pin_factory = PiGPIOFactory(host='192.168.7.96')
 robot = Robot(left=(7,8), right=(9,10))
+
 robot.stop()
 
 local_pins = PiGPIOFactory(host="localhost")
@@ -24,7 +23,6 @@ btn2 = Button(22, pin_factory=local_pins)
 btn3 = Button(13, pin_factory=local_pins)
 btn4 = Button(21, pin_factory=local_pins)
 led = LED(18, pin_factory=local_pins)
-
 
 def fwd():
     print('Go Forward')
